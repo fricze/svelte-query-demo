beefa45c24b6c24b2ebe73972213e817f29109f5
Andrzej Fricze <a.fricze@pm.me>
2023-10-17 21:20:28 +0200

    Documentation

--- src/lib/AppsContainer.svelte
+++ src/lib/AppsContainer.svelte
@@ -12,6 +12,16 @@
 		queryFn: () => getApps()
 	});
 
+	// Derived queries created based on example from docs: https://tanstack.com/query/latest/docs/svelte/reactivity
+	// Derived queries should be created when queryFn needs arguments from other query.
+	// If queryFn does not need data from other query it's best to avoid creating derived queries,
+	// for sake of simplicity. Always start with simple queries and queries invalidation.
+	// Remember that both query keys and derived queries create your graph of queries dependencies.
+	//
+	// Also, if one query needs data from two, or more, services that depend on each other
+	// it might be prefferable to call this services together, in one queryFn.
+	// Decision about creating derived queries should depend on actual elasticity and granularity
+	// you need in working with particular services
 	const appsDetailsQuery = createQuery(
 		derived(appsQuery, ($apps) => ({
 			queryKey: ['app-details'],
